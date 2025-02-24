b93e7e9cfcfba508cce5b829f0ab12f3b38407bb
FakeGods <w.leszkowicz99@gmail.com>
2025-02-24 23:07:04 +0100

    style button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,7 +5,7 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
   selector: 'app-header',
   imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-slate-100 px-4 py-3 shadow-md">
+    <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between">
       <span>My store </span>
       <app-primary-button />
     </div>
