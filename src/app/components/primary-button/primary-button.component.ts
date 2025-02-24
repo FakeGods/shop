71880284540c9c24dd01ba8c43ae213b6c9fa72d
FakeGods <w.leszkowicz99@gmail.com>
2025-02-24 23:09:08 +0100

    button

--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-primary-button',
   imports: [],
-  templateUrl: './primary-button.component.html',
-  styleUrl: './primary-button.component.css'
+  template: ` <button></button> `,
+  styleUrl: './primary-button.component.css',
 })
-export class PrimaryButtonComponent {
-
-}
+export class PrimaryButtonComponent {}
