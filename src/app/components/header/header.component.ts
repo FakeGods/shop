75e1800de25b7ef8de60e1b5e8598fccbe482834
FakeGods <w.leszkowicz99@gmail.com>
2025-02-28 00:04:08 +0100

    styling span and button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,8 +5,10 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
   selector: 'app-header',
   imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between">
-      <span>My store </span>
+    <div
+      class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center"
+    >
+      <span class="text-xl">My store </span>
       <app-primary-button label="Cart" />
     </div>
   `,
--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -3,7 +3,14 @@ import { Component, input, output } from '@angular/core';
 @Component({
   selector: 'app-primary-button',
   imports: [],
-  template: ` <button (click)="btnClicked.emit()">{{ label() }}</button> `,
+  template: `
+    <button
+      class="bg-green-600 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90"
+      (click)="btnClicked.emit()"
+    >
+      {{ label() }}
+    </button>
+  `,
   styleUrl: './primary-button.component.css',
 })
 export class PrimaryButtonComponent {
