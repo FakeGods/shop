8345b6fa3a6befb1a53a489ff5f8200979264214
FakeGods <w.leszkowicz99@gmail.com>
2025-03-01 23:27:34 +0100

    adding signal of products

--- src/app/pages/products-list/products-list.component.ts
+++ src/app/pages/products-list/products-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/products.model';
 
 @Component({
   selector: 'app-products-list',
   imports: [],
   templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.css'
+  styleUrl: './products-list.component.css',
 })
 export class ProductsListComponent {
-
+  products = signal<Product[]>([{}]);
 }
